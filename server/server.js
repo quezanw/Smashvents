a00f46ecc3e4108a2179460c69d4b3df06167cf0
quezan <quezanw@gmail.com>
2019-03-12 18:22:57 -0700

    intial express server

---
+++ server/server.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+const bodyParser = require('body-parser')
+const express = require('express')
+const session = require('express-session')
+// const passport = require('./config/passport')
+const cors = require('cors')
+const expressValidator = require('express-validator')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(expressValidator())
+app.use(cors())
+// app.use(passport.initialize())
+// app.use(passport.session())
+
+// require('./config/mongoose')
+// require('./config/routes')(app)
+
+app.listen(8000, err => err ? console.log(err) : console.log('listening on port 8000'))
