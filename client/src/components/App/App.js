53c3b47e086fb0ef22c6b010a474770ba25d253a
quezanw <quezanw@gmail.com>
2019-08-22 22:47:55 -0700

    sidebar touch bug

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -13,18 +13,6 @@ import EventCreate from '../Events/EventCreate/EventCreate';
 import styles from './App.module.scss';
 import '../../styles/main.scss';
 
-/* 
-create state for window size = null
-once app component mounts set state for window size
-if window size is under 900px width hide sidebar and display topbar
-
-if topbar is clicked show sidebar
-add event listener to window for scrolling.
-if window is scrolling change app state of isScrolling to true
-if isScrolling is true and state of window size is under 900px width hide sidebar
-
-*/
-
 class App extends React.Component {
   componentDidMount() {
     window.addEventListener('touchend', this.handleTouch);
@@ -32,11 +20,12 @@ class App extends React.Component {
 
   handleTouch = e => {
     let target = e.target;
-    let parent = target.offsetParent.id;
-    console.log(target);
-    if(window.innerWidth < 900 && target.id !== 'sidebar' && parent !== 'sidebar') {
-      document.getElementById('sidebar').style.opacity = 0;
-      document.getElementById('sidebar').style.visibility = 'hidden';
+    let parent = target.offsetParent;
+    if(parent) {
+      if(window.innerWidth < 900 && target.id !== 'sidebar' && parent.id !== 'sidebar') {
+        document.getElementById('sidebar').style.opacity = 0;
+        document.getElementById('sidebar').style.visibility = 'hidden';
+      }
     }
   }
 
@@ -50,8 +39,7 @@ class App extends React.Component {
     return (
       <div className={styles.container}>
         {this.renderModals()}
-        {/* {this.state.showSidbar ? <Sidebar/> : ''} */}
-        <Sidebar ref={this.sidebarRef} />
+        <Sidebar />
         <div className={styles.main}>
           <Topbar/>
           <Router history={history}>
