0dec09e22c5a3383aba2f33dc07a322362446688
quezanw <quezanw@gmail.com>
2019-09-03 21:51:53 -0700

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Smashvents
+
+### Description
+Smashvents is a web application used to host local Smash Bros Ultimate events in your community! 
+
+### Features
+- Login / Register for an account
+- View all upcoming and past events
+- View, Create, Edit, and Delete an event
+- Edit User profile settings
+- View venue location marked in google maps
+- Mobile device responsiveness
+- Form validations
+
+![](./examples/home.png)
+
+![](./examples/login_reg.png)
+
+![](./examples/user_settings.png)
+
+![](./examples/mobile.png)
+
+![](./examples/location.png)
+
+### Technologies
+- React
+- Redux
+- JavaScript
+- Sass
+- Express.js
+- PostgreSQL
--- client/src/components/EventPage/AttendeeCard/AttendeeCard.js
+++ client/src/components/EventPage/AttendeeCard/AttendeeCard.js
@@ -1,31 +1,24 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import styles from './AttendeeCard.module.scss';
 
-class AttendeeCard extends React.Component {
-
-  render() {
-    let user = this.props.user;
-    return ( 
-      <div className={styles.wrapper}>
-        <div style={{backgroundColor: user.theme_color}}className={styles.playerIcon}>
-          <p>{user.username.charAt(0)}</p>
-        </div>
-        <div className={styles.playerDetails}>
-          <p className={styles.username}>
-          {user.username}
-          </p>
-          <p className={styles.fullname}>
-            {user.first_name} {user.last_name}
-          </p>
-        </div>
+const AttendeeCard = props =>  {
+  let user = props.user;
+  
+  return ( 
+    <div className={styles.wrapper}>
+      <div style={{backgroundColor: user.theme_color}} className={styles.playerIcon}>
+        <p>{user.username.charAt(0)}</p>
       </div>
-    );
-  }
+      <div className={styles.playerDetails}>
+        <p className={styles.username}>
+        {user.username}
+        </p>
+        <p className={styles.fullname}>
+          {user.first_name} {user.last_name}
+        </p>
+      </div>
+    </div>
+  );
 }
 
-// const mapStateToProps = state => {
-
-// }
-
-export default connect(null)(AttendeeCard)
+export default AttendeeCard;
