aae430829c916adb2238d7bb854434db3e285ceb
quezanw <quezanw@gmail.com>
2019-09-06 19:56:44 -0700

    fixed venue map render logic

--- client/src/components/EventPage/VenueMap/VenueMap.js
+++ client/src/components/EventPage/VenueMap/VenueMap.js
@@ -6,32 +6,35 @@ import Marker from '../Marker/Marker';
 
 const VenueMap = props => {
   let { online, coords } = props.event;
-  if(!online && coords) {
-    let center = { lat: coords.lat, lng: coords.lng }
-    let zoom = 12;
-    return (
-      <div className={styles.largeOuterContainer}>
-        <h1>Location</h1>
-        <div className={styles.container}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
-            defaultCenter={center}
-            defaultZoom={zoom}
-          >
-            <Marker lat={coords.lat} lng={coords.lng} /> 
-          </GoogleMapReact>
+  if(!online) {
+    if(coords) {
+      let center = { lat: coords.lat, lng: coords.lng }
+      let zoom = 12;
+      return (
+        <div className={styles.largeOuterContainer}>
+          <h1>Location</h1>
+          <div className={styles.container}>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+              defaultCenter={center}
+              defaultZoom={zoom}
+            >
+              <Marker lat={coords.lat} lng={coords.lng} /> 
+            </GoogleMapReact>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
+    return (
+      <ClipLoader 
+        sizeUnit={"px"}
+        size={150}
+        color={'#123abc'}
+        loading={true}
+      />
+    );
   }
-  return (
-    <ClipLoader 
-      sizeUnit={"px"}
-      size={150}
-      color={'#123abc'}
-      loading={true}
-    />
-  );
+  return '';
 }
 
 export default VenueMap;
