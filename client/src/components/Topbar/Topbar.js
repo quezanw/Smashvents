6ba4d687d943f46c20861166b83599cf910c3487
quezanw <quezanw@gmail.com>
2019-09-25 20:45:54 -0700

    smashvents from top bar routes to home on click

--- client/src/components/Topbar/Topbar.js
+++ client/src/components/Topbar/Topbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Topbar.module.scss';
+import history from '../../history';
 
 class Topbar extends React.Component {
   toggleSidebar = (e) => {
@@ -7,11 +8,16 @@ class Topbar extends React.Component {
     document.getElementById('sidebar').style.visibility = 'visible';
     document.getElementById('sidebar').style.opacity = 1;
   }
+
+  goHome = () => {
+    history.push('/');
+  }
+
   render() {
     return (
       <div className={styles.topbar}>
         <i onClick={this.toggleSidebar} className={`${styles.menu} fas fa-bars`}></i>
-        <h1>SMASHVENTS</h1>
+        <h1 onClick={this.goHome}>SMASHVENTS</h1>
       </div>
     );
   }
